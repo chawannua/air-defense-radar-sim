13fe09d97f916baa9b9e8cdb680cea4172150c58
Chawannua <chawannua@gmail.com>
2026-09-09 02:51:31 +0700

    chore(release): bump version to 1.2.0

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 class GameConfig:
-    VERSION = "1.1.1"
+    VERSION = "1.2.0"
 
     # Real-world RTAF airbase positions in relative km from Bangkok (harmonized with geodata)
     AIRBASES = [
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 import sys
 from radar_ui import start_radar
 
